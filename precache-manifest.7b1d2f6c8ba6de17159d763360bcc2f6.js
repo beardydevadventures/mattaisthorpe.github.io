ff8b65f04d5f545b85159b275b20d9742b56b0b0
Matthew Aisthorpe <m.aisthorpe@hotmail.com>
2019-12-31 01:24:22 +1000

    Updated fromuser-page-build branch

---
+++ precache-manifest.7b1d2f6c8ba6de17159d763360bcc2f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6dcb6a0aede0adec5181a7481b92da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeb0e6e62e5d2386713d",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1f75d5040d629c1a8896",
+    "url": "/static/js/2.dafc8fe4.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.dafc8fe4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eeb0e6e62e5d2386713d",
+    "url": "/static/js/main.907e205d.chunk.js"
+  },
+  {
+    "revision": "a537087786f72bc5e016",
+    "url": "/static/js/runtime-main.2033fc0a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mattyace.github.io/precache-manifest.ae69c3f8a0365af07fe19234acd4f3db.js"
+  "/precache-manifest.7b1d2f6c8ba6de17159d763360bcc2f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mattyace.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
