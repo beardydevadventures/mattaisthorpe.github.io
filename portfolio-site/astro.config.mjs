37103ea21925c3969e27d067bb61282cc0236481
Matthew Aisthorpe <m.aisthorpe@hotmail.com>
2024-05-09 01:39:53 +1000

    updated url and redirects for old blog urls

--- portfolio-site/astro.config.mjs
+++ portfolio-site/astro.config.mjs
@@ -5,7 +5,21 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
 	  	integrations: [mdx()],
-		site: 'https://beardydevadventures.github.io',
-		base: 'mattaisthorpe.github.io',
+		site: 'https://matthewaisthorpe.com.au',
+		//base: 'mattaisthorpe.github.io',
+		redirects: {
+			'/vr/streaming-oculusmeta-quest-2-to-discord': {
+			  status: 301,
+			  destination: '/posts/vr/stream-quest-to-discord',
+			},
+			'/hosting/host-unity-projects-on-github-pages': {
+			  status: 301,
+			  destination: '/posts/unity/host-unity-github-pages',
+			},
+			'/code/json-object-vs-json-array': {
+			  status: 301,
+			  destination: '/posts/code/json-object-vs-array',
+			},
+		}	
 	},
 );
