bb26d1371b327676b16a85505be7ad8de597ae44
Matthew Aisthorpe <m.aisthorpe@hotmail.com>
2024-05-26 17:37:41 +1000

    Added sitemap and google search file

--- portfolio-site/astro.config.mjs
+++ portfolio-site/astro.config.mjs
@@ -1,25 +1,25 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	  	integrations: [mdx()],
-		site: 'https://matthewaisthorpe.com.au',
-		//base: 'mattaisthorpe.github.io',
-		redirects: {
-			'/vr/streaming-oculusmeta-quest-2-to-discord': {
-			  status: 301,
-			  destination: '/posts/vr/stream-quest-to-discord',
-			},
-			'/hosting/host-unity-projects-on-github-pages': {
-			  status: 301,
-			  destination: '/posts/unity/host-unity-github-pages',
-			},
-			'/code/json-object-vs-json-array': {
-			  status: 301,
-			  destination: '/posts/code/json-object-vs-array',
-			},
-		}	
-	},
-);
+  integrations: [mdx(), sitemap()],
+  site: 'https://matthewaisthorpe.com.au',
+  //base: 'mattaisthorpe.github.io',
+  redirects: {
+    '/vr/streaming-oculusmeta-quest-2-to-discord': {
+      status: 301,
+      destination: '/posts/vr/stream-quest-to-discord'
+    },
+    '/hosting/host-unity-projects-on-github-pages': {
+      status: 301,
+      destination: '/posts/unity/host-unity-github-pages'
+    },
+    '/code/json-object-vs-json-array': {
+      status: 301,
+      destination: '/posts/code/json-object-vs-array'
+    }
+  }
+});
