688402ac03a036fc2cc3550470a09e0c97127ead
Matthew Aisthorpe <m.aisthorpe@hotmail.com>
2024-05-26 22:21:24 +1000

    Styling update and added activity page
    Added tailwind.css to project, moved readme to root and created activity page, combining feeds from YouTube and GitHub.

---
+++ portfolio-site/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+  './src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: ['selector','theme-dark'],
+  plugins: [],
+}
