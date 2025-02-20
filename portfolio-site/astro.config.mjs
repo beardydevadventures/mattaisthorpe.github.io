6d7d68eb5c407ec990cab8e27a5a81a994e5d393
Matthew Aisthorpe <m.aisthorpe@hotmail.com>
2025-02-20 16:32:24 +1000

    Updated the astro config for partytown

--- portfolio-site/astro.config.mjs
+++ portfolio-site/astro.config.mjs
@@ -5,21 +5,29 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
-  site: 'https://matthewaisthorpe.com.au',
-  //base: 'mattaisthorpe.github.io',
-  redirects: {
-    '/vr/streaming-oculusmeta-quest-2-to-discord': {
-      status: 301,
-      destination: '/posts/vr/stream-quest-to-discord'
-    },
-    '/hosting/host-unity-projects-on-github-pages': {
-      status: 301,
-      destination: '/posts/unity/host-unity-github-pages'
-    },
-    '/code/json-object-vs-json-array': {
-      status: 301,
-      destination: '/posts/code/json-object-vs-array'
+    integrations: [
+        mdx(),
+        sitemap(),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+    ],
+    site: 'https://matthewaisthorpe.com.au',
+    //base: 'mattaisthorpe.github.io',
+    redirects: {
+        '/vr/streaming-oculusmeta-quest-2-to-discord': {
+            status: 301,
+            destination: '/posts/vr/stream-quest-to-discord'
+        },
+        '/hosting/host-unity-projects-on-github-pages': {
+            status: 301,
+            destination: '/posts/unity/host-unity-github-pages'
+        },
+        '/code/json-object-vs-json-array': {
+            status: 301,
+            destination: '/posts/code/json-object-vs-array'
+        }
     }
-  }
 });
